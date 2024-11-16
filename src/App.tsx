98e68ea8ffb321b476f5c68c55ba663bb7a5ef43
AliakseiKrasko <alekskrasko1985@gmail.com>
2024-11-16 11:58:48 +0300

    added component accordion

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import {Message} from './Message';
 import {OnOf} from './components/onOf/OnOf';
 import {Rating} from './components/rating/Rating';
+import {Accordion} from './components/accordion/Accordion';
 
 type Task = {
     id: number
@@ -102,6 +103,9 @@ function App() {
             <Rating value={3} />
             <Rating value={4} />
             <Rating value={5} />
+            <Accordion />
+            <Accordion />
+            <Accordion />
         </div>
     );
 }
